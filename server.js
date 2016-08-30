f0ada618cfded73b86029bb40b7fbf737a41a522
Nick Balestra <nbalestra@opentable.com>
2016-08-30 19:58:21 +0100

    programmatically bundle via jspm.api

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
+const app = require('express')()
 const hml = process.env.HML || false
 const serveApp = require('./middleware')({endPoint: '/boom', hml})
+const bundle = require('jspm').bundle
+const bundleOptions = {
+  mangle: true,
+  minify: true,
+  injectConfig:true,
+  lowResSourceMaps: true,
+  sourceMaps: true
+}
+let server
 
 app.use(serveApp)
-const server = app.listen(8080, () => 
-  console.log(`👂 Server listening on port ${server.address().port}`)
-)
+bundle('index.js', 'app/build.js', bundleOptions)
+  .then(() => {
+    server = app.listen(8080, () => 
+      console.log(`👂 Server listening on port ${server.address().port}`)
+    )
+  })
 
 if (hml) require('chokidar-socket-emitter')({port: 5776})
